f8713c5d03a416a40f60b21b8e2f146fdc471f0f
kedarnath48 <kedarnath661@gmail.com>
2026-07-13 22:24:44 +0530

    feat: add PaletteModel component and integrate with dialog functionality in App

---
+++ src/ui/components/paletteModel.tsx
@@ -0,0 +1,12 @@
+
+const PaletteModel = () => {
+  return (
+    <div>
+      <div className="input-container">
+        <input type="text" />
+      </div>
+    </div>
+  )
+}
+
+export default PaletteModel
